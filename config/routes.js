c5726ecb7ab32f6a408689104a15a5810271b246
tjforeman <tylerforeman92@gmail.com>
2019-05-10 11:45:25 -0400

    register and login both working

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,10 @@
 const axios = require('axios');
 
+const bcrypt=require('bcryptjs')
+const db =require('../database/dbConfig.js');
+const jwt =require('jsonwebtoken')
+const secrets=require('./secret.js')
+
 const { authenticate } = require('../auth/authenticate');
 
 module.exports = server => {
@@ -10,10 +15,37 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+    let user=req.body 
+    const hash=bcrypt.hashSync(user.password,10)
+    user.password=hash
+    db('users')
+    .insert(req.body)
+    .then(user =>{
+        const token=generateToken(user)
+        res.status(200).json({message:`Welcome ${user.username}, you have successfully singed up`,token})
+        })
+    .catch(err=>{
+        res.status(500).json({error:err,message:'Unable to sign up at this time'})
+    })
 }
 
 function login(req, res) {
   // implement user login
+    let {username,password}=req.body 
+    db('users')
+    .where({username})
+    .first()
+    .then(user =>{
+        if(user&& bcrypt.compareSync(password,user.password)){
+            const token=generateToken(user)
+        res.status(200).json({message:`Welcome ${user.username}, you have successfully logged in`,token})
+        }else{
+            res.status(401).json({message:'you shall not pass!'})
+        }
+        })
+    .catch(err=>{
+        res.status(500).json({error:err,message:'Unable to log in at this time'})
+    })
 }
 
 function getJokes(req, res) {
@@ -30,3 +62,14 @@ function getJokes(req, res) {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
 }
+
+function generateToken(user){
+  const payload={
+      subject: user.id,
+      username:user.username
+  };
+  const options={
+      expiresIn:'1h'
+  };
+      return jwt.sign(payload, secrets.jwtSecret, options)
+  }
